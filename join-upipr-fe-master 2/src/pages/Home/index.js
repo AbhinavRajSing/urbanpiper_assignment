016026ce73295599fbf194b9e5b21fa26afb2056
Abhi <abhi@gmail.com>
2021-05-29 15:03:13 +0530

    changes

--- join-upipr-fe-master 2/src/pages/Home/index.js
+++ join-upipr-fe-master 2/src/pages/Home/index.js
@@ -8,8 +8,9 @@ import Person from '../Person';
 
 function HomePage() {
 
-  const [data,setData] = React.useState()
-
+  const [data,setData] = React.useState([])
+  const [active, setActive] = React.useState(0);
+  const [q,setQ] = React.useState("")
 
   
 
@@ -24,11 +25,14 @@ function debounce(e){
   let {value} = e.target
   x && clearTimeout(x)
   x=setTimeout(()=>{
-  fetchData(value)
+    fetchData(value)
   },500)
 }
-
-
+const handleClear=(e)=>{
+  setQ("")
+  setData([])
+  
+}
 
 
 
@@ -37,15 +41,20 @@ function debounce(e){
       <div className="logo">
         <img src={logo} alt="Star Wars Logo" />
       </div>
-      <input className="search-input" placeholder="Search by name" onChange={(e)=>debounce(e)}/>
-      {/* <span className="span"><FaSearch className="inputicons"/></span> */}
-      {
+      <div className="wrapper">
+        <input className="search-input" placeholder="Search by name" onChange={(e)=>{debounce(e);setQ(e.target.value)}} value={q}/>
+        <span className="span">{!q ?<FaSearch className="inputicons"/>:
+        <span className="clear" onClick={(e)=>handleClear(e)}>x</span>}</span>
+      </div>
+      <div className="dropbox" >{
         data?.map(elm=>{
           return (
-          <Link to={`person/${elm.name}`}><p>{elm.name}</p></Link>
+            <Link to={`person/${elm.name}`}>
+              <p >{elm.name}</p>
+            </Link>
           )
         })
-      }
+      }</div>
     </div>
   );
 }
